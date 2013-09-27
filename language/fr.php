b762dd0b066923e858dafbb24e8525fc77b500a5
Mobidiom <contact@mobidiom.com>
2013-09-27 20:51:52 +0200

    Create fr.php
    Creation of french language file

---
+++ language/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'login_title'       => 'Connectez-vous',
+    'login_username'    => 'Identifiant',
+    'login_password'    => 'Mot de passe',
+    'login_btn'         => 'Connexion',
+    'task_title'        => 'Merci de choisir la tâche que vous souhaitez commencer',
+    'choose_project'    => 'Choisir un projet :',
+    'choose_task'       => 'Choisir une tâche :',
+    'stop'              => 'Arrêter',
+    'start'             => 'Démarrer',
+    'error'             => 'Erreur',
+    'error_tasks'       => 'Désolé, mais la liste des tâches ne peut être chargée',
+    'error_projects'    => 'Désolé, mais la liste des projets ne peut être chargée',
+    'working_time'      => 'Vous travaillez à présent : {0}'
+);
