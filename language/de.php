2c14ac1d1b021ccbd03834f0ccd50e615b30fd8f
Kevin Papst <kpapst@gmx.net>
2016-03-10 00:14:55 +0100

    added german translation

---
+++ language/de.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'login_title'       => 'Bitte anmelden',
+    'login_username'    => 'Benutzername',
+    'login_password'    => 'Passwort',
+    'login_btn'         => 'Anmelden',
+    'task_title'        => 'Bitte wählen Sie die zu startenden Aktivität aus',
+    'choose_project'    => 'Prjekt auswählen:',
+    'choose_task'       => 'Aktivität auswählen:',
+    'stop'              => 'Stop',
+    'start'             => 'Start',
+    'error'             => 'Fehler',
+    'error_tasks'       => 'Entschuldigung, ein Fehler trat beim Laden der Aktivitäten auf',
+    'error_projects'    => 'Entschuldigung, ein Fehler trat beim Laden der Projekte auf',
+    'working_time'      => 'Du arbeitest jetzt: {0}'
+);
